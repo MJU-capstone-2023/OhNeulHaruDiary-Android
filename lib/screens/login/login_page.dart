d8e3ee5ec1d99a04f272be1115d07e7d825362c5
wkdyujin <yujin1203713@gmail.com>
2023-04-28 12:28:30 +0900

    [FEAT] login page ui 생성

--- lib/screens/login/login_page.dart
+++ lib/screens/login/login_page.dart
@@ -9,34 +9,105 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Page'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              child: const Text('로그인'),
-              onPressed: () {
-                // 로그인 버튼 클릭 시 처리할 내용
-                Navigator.pushReplacement( // 현재 route를 스택에서 제거
-                  context,
-                  MaterialPageRoute(builder: (context) => MainPage()),
-                );
-              },
+      body: Column(
+        children: [
+          const SizedBox(height: 132),
+          SizedBox(
+            height: 80,
+            child: Image.asset('images/sketch_day_logo.png'), // 로고 이미지 경로
+          ),
+          const SizedBox(height: 76),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: '아이디 입력',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide: const BorderSide(width: 1),
+                ),
+              ),
             ),
-            ElevatedButton(
-              child: const Text('회원가입'),
+          ),
+          const SizedBox(height: 12),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: '비밀번호 입력',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide: const BorderSide(width: 1),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 4),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+                children: [
+                  Checkbox(
+                    value: false,
+                    onChanged: (value) {},
+                  ),
+                  const Text('로그인 상태 유지'),
+                ],
+              ),
+            ),
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: ElevatedButton(
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => SignUpPage()),
+                  MaterialPageRoute(builder: (context) => const MainPage()),
                 );
               },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5), // 모서리 둥글기
+                ),
+                minimumSize: Size(MediaQuery.of(context).size.width, 50), // 최소 크기
+              ),
+              child: const Text('로그인'),
             ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 12),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: OutlinedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SignUpPage()),
+                );
+              },
+              style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                side: const BorderSide(color: Colors.indigo, width: 1),
+                minimumSize: Size(MediaQuery.of(context).size.width, 50), // 최소 크기
+              ),
+              child: const Text('회원가입'),
+            ),
+          ),
+          const SizedBox(height: 24),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  '비밀번호 찾기',
+                  style: TextStyle(decoration: TextDecoration.underline),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
