1cec7a183bd64fbf02daef8a5c1121984b13dad0
wkdyujin <yujin1203713@gmail.com>
2023-05-16 13:26:28 +0900

    [FEAT] 일기 작성 페이지 생성

--- lib/screens/main/create_post/write_diary_page.dart
+++ lib/screens/main/create_post/write_diary_page.dart
@@ -1,13 +1,130 @@
 import 'package:flutter/material.dart';
+import 'package:sketch_day/screens/main/create_post/view_diary_page.dart';
 
-class WriteDiaryPage extends StatelessWidget {
-  const WriteDiaryPage({Key? key}) : super(key: key);
+class WriteDiaryPage extends StatefulWidget {
+  @override
+  _WritePageState createState() => _WritePageState();
+}
+
+class _WritePageState extends State<WriteDiaryPage> {
+  DateTime selectedDate = DateTime.now(); // 선택된 날짜를 저장하기 위한 변수
+
+  void showDatePickerDialog() async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+
+    if (pickedDate != null && pickedDate != selectedDate) {
+      setState(() {
+        selectedDate = pickedDate;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('WriteDiaryPage'),
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 40.0, left: 10.0, right: 10.0),
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context); // 이전 페이지로 돌아가기
+                  },
+                  icon: const Icon(Icons.close),
+                ),
+                const Spacer(),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text(
+                    '저장',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(width: 10.0),
+                Text(
+                  '${selectedDate.year}년 ${selectedDate.month}월 ${selectedDate.day}일',
+                  style: const TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    showDatePickerDialog();
+                  },
+                  icon: const Icon(Icons.arrow_drop_down),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    // 날씨 다이얼로그 표시
+                  },
+                  icon: const Icon(Icons.wb_sunny),
+                ),
+                IconButton(
+                  onPressed: () {
+                    // 기분 다이얼로그 표시
+                  },
+                  icon: const Icon(Icons.emoji_emotions),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: TextField(
+                expands: true,
+                maxLines: null,
+                textAlignVertical: TextAlignVertical.top,
+                decoration: InputDecoration(
+                  hintText: '오늘 하루를 기록해보세요!',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    borderSide: const BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    borderSide: const BorderSide(color: Colors.grey),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    borderSide: const BorderSide(color: Colors.grey),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
