f68190006064fe84823daeff505ea33a3e9efe9f
wkdyujin <yujin1203713@gmail.com>
2023-05-23 17:53:52 +0900

    [FEAT] 일기 조회 비즈니스 로직

--- lib/screens/main/create_post/view_diary_page.dart
+++ lib/screens/main/create_post/view_diary_page.dart
@@ -1,59 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class ViewDiaryPage extends StatelessWidget {
-  final DateTime selectedDate;
-  final String diaryContent;
+class ViewDiaryPage extends StatefulWidget {
+  final int diaryId;
 
-  ViewDiaryPage({super.key, required this.selectedDate, required this.diaryContent});
+  ViewDiaryPage({Key? key, required this.diaryId}) : super(key: key);
+
+  @override
+  _ViewDiaryPageState createState() => _ViewDiaryPageState();
+}
+
+class _ViewDiaryPageState extends State<ViewDiaryPage> {
+  Future<Map<String, dynamic>>? _diary;
+
+  @override
+  void initState() {
+    super.initState();
+    _diary = _fetchDiary();
+  }
+
+  Future<Map<String, dynamic>> _fetchDiary() async {
+    final response = await http.get(
+      Uri.parse('${dotenv.env['BASE_URL']}/diary/login/${widget.diaryId}'),
+    );
+    final diary = jsonDecode(response.body);
+    return diary;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 40.0, left: 10.0, right: 10.0),
-            child: Row(
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: _diary,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Column(
               children: [
-                IconButton(
-                  onPressed: () {
-                    Navigator.pop(context); // 이전 페이지로 돌아가기
-                  },
-                  icon: const Icon(Icons.close),
+                Padding(
+                  padding: const EdgeInsets.only(top: 40.0, left: 10.0, right: 10.0),
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.pop(context); // 이전 페이지로 돌아가기
+                        },
+                        icon: const Icon(Icons.close),
+                      ),
+                    ],
+                  ),
                 ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(width: 10.0),
-                Text(
-                  '${selectedDate.year}년 ${selectedDate.month}월 ${selectedDate.day}일',
-                  style: const TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold,
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(width: 10.0),
+                      Text(
+                        snapshot.data!['date'], // Assuming 'date' is a key in your response
+                        style: const TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                    child: SingleChildScrollView(
+                      child: Text(
+                        snapshot.data!['content'], // Assuming 'content' is a key in your response
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ],
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-              child: SingleChildScrollView(
-                child: Text(
-                  diaryContent,
-                  style: const TextStyle(
-                    fontSize: 16.0,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
